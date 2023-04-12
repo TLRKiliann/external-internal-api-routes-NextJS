cae95038d5b43a7f529d6d8f3405f1ac2632c50e
TLRKiliann <philogenie@protonmail.com>
2023-04-12 09:28:18 +0200

    config redirects done

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: async () => {
+    return [
+      {
+        source: '/enterall',
+        destination: '/',
+        permanent: false,
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
